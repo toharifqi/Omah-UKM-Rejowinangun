361524dd1217e6d57852808aa91a586225d5dd33
toharifqi <rifqinaufaltohari@gmail.com>
2020-08-07 10:51:16 +0700

    routine push 10:50 (8/72020)

--- app/src/main/java/com/toharifqi/um/ukmq/EditUkmActivity.java
+++ app/src/main/java/com/toharifqi/um/ukmq/EditUkmActivity.java
@@ -1,31 +1,14 @@
 package com.toharifqi.um.ukmq;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
 public class EditUkmActivity extends AppCompatActivity {
-    Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_ukm);
-
-        // Action Bar
-        mToolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(mToolbar);
-        getSupportActionBar().setDisplayShowTitleEnabled(false);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setDisplayShowHomeEnabled(true);
-        getSupportActionBar().setHomeAsUpIndicator(R.drawable.close);
-
-    }
-
-    @Override
-    public boolean onSupportNavigateUp() {
-        onBackPressed();
-        return true;
     }
 }
